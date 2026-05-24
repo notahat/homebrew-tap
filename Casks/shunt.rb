a7e381e68108f3106b3a139fd526f42a9b0fc056
Pete Yandell <pete@notahat.com>
2026-05-24 23:55:27 +1000

    Update Shunt to v1.2.3

--- Casks/shunt.rb
+++ Casks/shunt.rb
@@ -1,6 +1,6 @@
 cask "shunt" do
-  version "1.2.2"
-  sha256 "6ebb7ed81b7ab9315dfc1fd75b4b387935e15a804f5fc71353a777d24ae53428"
+  version "1.2.3"
+  sha256 "78ce20197173be5d70b33a06aa4b3199aeeb3d7b55d9c3a341fa373832724b5b"
 
   url "https://github.com/notahat/Shunt/releases/download/v#{version}/Shunt-#{version}.zip"
   name "Shunt"
